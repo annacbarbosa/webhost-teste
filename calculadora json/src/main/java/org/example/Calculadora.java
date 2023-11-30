f2581fabeb16d78952a158878e7888ffaff1e778
anna <127499380+annacbarbosa@users.noreply.github.com>
2023-11-30 12:19:41 -0300

    Add files via upload

---
+++ calculadora json/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Calculadora {
+
+    public double Somar (double a, double b) {
+        return (a + b);
+    }
+    public double Subtrair (double a, double b) {
+        return (a - b);
+    }
+    public double Multiplicar (double a, double b) {
+        return (a * b);
+    }
+    public double Dividir (double a, double b) {
+        return (a / b);
+    }
+
+}
